bd474c1fdfce4de60ffc5093e9d579890cffe0dd
khimalAnmal-design <khimal081130@student.jenseneducation.se>
2026-05-18 09:00:08 +0200

    a
    bootstrap

--- init.js
+++ init.js
@@ -1,1 +1,11 @@
  // Extrauppgift: Fixa så att headern och footern laddas in i varje sida från denna fil
+
+ document.body.insertAdjacentHTML(
+  "beforeend",
+
+  `
+  <footer class="bg-dark text-white text-center p-4">
+    Footer
+  </footer>
+  `
+)
