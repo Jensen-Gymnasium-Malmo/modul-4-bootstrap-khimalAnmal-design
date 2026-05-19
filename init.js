76e7acf7228eff3ea1000ed75c8667f7819dfc4f
khimalAnmal-design <khimal081130@student.jenseneducation.se>
2026-05-19 14:06:00 +0200

    hemsida
    commit ej klar än

--- init.js
+++ init.js
@@ -1,11 +1,3 @@
- // Extrauppgift: Fixa så att headern och footern laddas in i varje sida från denna fil
 
- document.body.insertAdjacentHTML(
-  "beforeend",
 
-  `
-  <footer class="bg-dark text-white text-center p-4">
-    Footer
-  </footer>
-  `
-)
+console.log('Sidan har laddats!');
